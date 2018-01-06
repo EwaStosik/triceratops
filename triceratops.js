bc878aff87097b94a2f2903a5a0fbb284e3348b6
Ewa Stosik <e.stosik@outlook.com>
2018-01-06 20:44:59 +0100

    First correction

--- triceratops.js
+++ triceratops.js
@@ -1,12 +1,12 @@
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 var dinosaur = 'triceratops';
 var dinosaurUpperCased = dinosaur.toUpperCase();
-var textChanged = text.replace('Velociraptor', dinosaur);
+var textChanged = text.replace('Velociraptor', dinosaurUpperCased);
 
 console.log(textChanged);
 console.log(textChanged.length);
 textHalf = (textChanged.length/2);
-textNew = text.substr(0, textHalfLength);
+textNew = text.substr(0, var textHalf);
 
 console.log(textNew);
 console.log(textNew.length);
